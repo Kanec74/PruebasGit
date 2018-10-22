6e582fa89892ac567ab968ba3d6f3b08d60911e4
kanec74 <kanec1@hotmail.com>
2018-10-22 10:02:27 -0500

    Se crea atributo persona.nombre

--- git/src/main/java/uady/mx/git/domain/Persona.java
+++ git/src/main/java/uady/mx/git/domain/Persona.java
@@ -2,4 +2,15 @@ package uady.mx.git.domain;
 
 public class Persona {
 
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+
 }
